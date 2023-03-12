c143d5adf3853c4dfa9b57a41556891f3f5cf676
Shiv Sharma <shivsharmavictory@gmail.com>
2023-03-12 13:53:29 +0530

    config and dependencies added

--- products/index.js
+++ products/index.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT | 4001;
+const port = process.env.PORT | 4002;
 
 // middlewares
 app.use(express.json());
 
 app.use('/', (req, res) => {
     return res.json({
-        message: 'hello from the customer service'
+        message: 'hello from the products service'
     });
 });
 
@@ -19,6 +19,6 @@ app.listen(port, (err) => {
     if(err) {
         console.log(`Facing issue in starting server: ${err}`);
     } else {
-        console.log(`customer service is listening at port ${port}`);
+        console.log(`products service is listening at port ${port}`);
     }
 });
